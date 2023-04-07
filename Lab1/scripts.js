262849a26d4c19dacc17b3fcd30767db5e9305dd
Denys Bartosevych <denys.bartosevych.pz.2021@lpnu.ua>
2023-04-07 11:44:54 +0300

    Lab2 Added working editing and adding forms. Removed unworking paging

--- Lab1/scripts.js
+++ Lab1/scripts.js
@@ -1,50 +1,51 @@
-var linesFilled = 1;
 
 function addStudent()
 {
-    if(linesFilled >= 4)
-        return;
-
-    var cellText = [ '<input type = checkbox>',
-    'PZ-24',
-    'Markiv Olena',
-    'F',
-    '18.01.2005',
-    '<input type= checkbox class = "status-button"/>',
-    '<button style = "background-image: url(\'pencil.png\'); background-size:auto 50%; background-repeat: no-repeat; background-position: center;"></button><button onclick="deleteStudent(this)" style = "background-image: url(\'delete.png\'); background-size:auto 50%; background-repeat: no-repeat; background-position: center; margin-left: 4px"></button>'];
-
-    var rows = document.querySelectorAll('#TableBody tr');
-    var cells = rows[linesFilled%4].querySelectorAll('th');
-
+    var cellText = getValues()
+    var rows = document.getElementById("TableBody").insertRow(-1);
+    var cells = []
+    for(var i = 0; i < cellText.length; i++)
+    {
+        cells[i] = rows.insertCell(i);
+    }
     for(var i = 0; i < cells.length; i++){
         cells[i].innerHTML = cellText[i];
     }
-    linesFilled++;
+    resetInputs()
     closeAddStudent()
+}
+
+function editStudent(button)
+{
+  var row = button.parentNode.parentNode;
+  var cells = row.getElementsByTagName("td");
+  var cellText = getValues();
+  for(var i = 0; i < cells.length; i++){
+    cells[i].innerHTML = cellText[i];
+  }
+  resetInputs()
+  closeAddStudent()
 }
 
 function deleteStudent(button) {
   var row = button.parentNode.parentNode;
   row.parentNode.removeChild(row);
-  var rows = document.querySelectorAll('#TableBody tr');
-  if (rows.length > 1) {
-    var cells = rows[rows.length - 1].querySelectorAll('th');
-    var cellValues = [];
-    for (var i = 0; i < cells.length; i++) {
-      cellValues.push(cells[i].innerHTML);
-    }
-    var newRow = document.createElement('tr');
-    for (var i = 0; i < 7; i++) {
-      var newCell = document.createElement('th');
-      newRow.appendChild(newCell);
-    }
-    document.querySelector('#TableBody').appendChild(newRow);
-    cells = rows[rows.length - 1].querySelectorAll('th');
-    for (var i = 0; i < cells.length; i++) {
-      cells[i].innerHTML = cellValues[i];
-    }
+}
+
+function openAddStudent(){
+  const overlay = document.getElementById('add-student-overlay');
+  var myButton = document.getElementById('submit-button');
+  myButton.onclick = addStudent;
+  overlay.style.display = 'flex';
+}
+
+function openEditStudent(button){
+  const overlay = document.getElementById('add-student-overlay');
+  var myButton = document.getElementById('submit-button');
+  myButton.onclick = function(){
+    editStudent(button);
   }
-  linesFilled--;
+  overlay.style.display = 'flex';
 }
 
 function closeAddStudent(){
@@ -52,7 +53,23 @@ function closeAddStudent(){
   overlay.style.display = 'none';
 }
 
-function openAddStudent(){
-  const overlay = document.getElementById('add-student-overlay');
-  overlay.style.display = 'flex';
+
+function resetInputs()
+{
+  document.querySelector('.select-group').value = "";
+  document.querySelector('.first-name').value = "";
+  document.querySelector('.last-name').value = "";
+  document.querySelector('.gender').value = "";
+  document.querySelector('.birthday').value = "";
+}
+
+function getValues()
+{
+  return [ '<input type = checkbox>',
+    document.querySelector('.select-group').value,
+    document.querySelector('.first-name').value+" "+document.querySelector('.last-name').value,
+    document.querySelector('.gender').value == 'male'?"M":"F",
+    document.querySelector('.birthday').value,
+    '<input type= checkbox class = "status-button"/>',
+    '<button onclick="openEditStudent(this)" style = "background-image: url(\'pencil.png\'); background-size:auto 50%; background-repeat: no-repeat; background-position: center;"></button><button onclick="deleteStudent(this)" style = "background-image: url(\'delete.png\'); background-size:auto 50%; background-repeat: no-repeat; background-position: center; margin-left: 4px"></button>'];
 }
